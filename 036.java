c96cbcadb10bb630fc0cd6c5d9dbae635526a6e0
mahebei <31981108+mahebei@users.noreply.github.com>
2018-11-13 12:31:08 +1300

    Update 036.java

--- 036.java
+++ 036.java
@@ -1,4 +1,6 @@
 /*
+36. Valid Sudoku
+
 Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 1.Each row must contain the digits 1-9 without repetition.
 2.Each column must contain the digits 1-9 without repetition.
