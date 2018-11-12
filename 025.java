a810b7145821153ba75859616acff87079b55578
mahebei <31981108+mahebei@users.noreply.github.com>
2018-11-13 12:23:37 +1300

    Update 025.java

--- 025.java
+++ 025.java
@@ -1,4 +1,6 @@
 /*
+25. Reverse Nodes in k-Group
+
 Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
 
 k is a positive integer and is less than or equal to the length of the linked list. 
