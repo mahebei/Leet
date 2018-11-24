c3910e210d1b0b9c7831003529fb6e80e020fa54
mahebei <31981108+mahebei@users.noreply.github.com>
2018-11-25 11:59:07 +1300

    Create 094.java

---
+++ 094.java
@@ -0,0 +1,50 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	public List<Integer> inorder(TreeNode root, List<Integer> res) {
+		if (root != null) {
+			inorder(root.left, res);
+			res.add(root.val);
+			inorder(root.right, res);
+		}
+		return res;
+	}
+	public List<Integer> inorderTraversal(TreeNode root) {
+		//return inorder(root, new LinkedList<>());
+		List<Integer> res = new LinkedList<>();
+		Stack<TreeNode> stack = new Stack<>();
+		while (!stack.isEmpty() || root != null) {
+			if (root != null) {
+				stack.add(root);
+				root = root.left;
+			} else {
+				res.add(stack.peek().val);
+				root = root.right;
+			}
+		}
+		return res;
+	}
+}
