4c69e599776fd6bb55b19a102568e985dee35948
mahebei <31981108+mahebei@users.noreply.github.com>
2018-12-18 12:44:40 +1300

    Update and rename No219 to 219.java

--- 201-300/219.java
+++ 201-300/219.java
@@ -20,18 +20,14 @@ Output: false
 */
 
 class Solution {
-	public boolean containsNearbyDuplicate(int[] nums, int k) {
-		if (nums.length <= 1) return false;
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums.length <= 1) return false;
 		HashSet<Integer> set = new HashSet<>();
-		for (int i = 0; i <= Math.min(k, nums.length - 1); i++) {
-			if (set.contains(nums[i])) return true;
-			set.add(nums[i]);
-		}
-		for (int i = k + 1; i < nums.length; i++) {
-			set.remove(nums[i - k - 1]);
+		for (int i = 0; i < nums.length; i++) {
+			if (i > k) set.remove(nums[i - k - 1]);
 			if (set.contains(nums[i])) return true;
 			set.add(nums[i]);
 		}
 		return false;
-	}
+    }
 }
