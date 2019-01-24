dab25f016696d438ad9f0c34d5f4921bba6aea12
mahebei <31981108+mahebei@users.noreply.github.com>
2019-01-25 10:21:12 +1300

    Create 406.java

---
+++ 401-500/406.java
@@ -0,0 +1,43 @@
+/*
+406. Queue Reconstruction by Height
+Medium
+
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
+where h is the height of the person and k is the number of people in front of this person
+who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
+
+class Solution {
+	public int[][] reconstructQueue(int[][] people) {
+		if (people.length == 0) return new int[0][0];
+		Arrays.sort(people, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) return o1[1] - o2[1];
+				return o2[0] - o1[0];
+			}
+		});
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < people.length; i++) {
+			list.add(people[i][1], people[i]);
+		}
+		int[][] res = new int[people.length][2];
+		for (int i = 0; i < people.length; i++) {
+			res[i][0] = list.get(i)[0];
+			res[i][1] = list.get(i)[1];
+		}
+		return res;
+	}
+}
