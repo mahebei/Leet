3932cd9735c5179e7584e86b704201abedf9d456
mahebei <31981108+mahebei@users.noreply.github.com>
2019-01-29 11:29:39 +1300

    Update 453.java

--- 401-500/453.java
+++ 401-500/453.java
@@ -1,5 +1,6 @@
 /*
 453. Minimum Moves to Equal Array Elements
+Easy
 
 Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal,
 where a move is incrementing n - 1 elements by 1.
