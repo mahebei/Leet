153606217d4ee2348af4ecbd633386aef60508f2
mahebei <31981108+mahebei@users.noreply.github.com>
2019-02-02 09:57:38 +1300

    Create 460.java

---
+++ 401-500/460.java
@@ -0,0 +1,71 @@
+/*
+This problem uses 3 maps, one is to store the key-value pair given by user.
+One is to store the count of how many times used by each key. (key-count)
+One is to store count-HashSet, count is the time used of each key,
+	HashSet is to store keys used for corresponding times(by order).
+Integer min is to store current min times used of all keys.
+
+For get method,
+if map not contain the given key, return -1,
+else find the time used of this key, update the time in the 2 maps about count.
+	if the time == min and no other keys used for min times, update min as well.
+
+For set method,
+if cap <=0 return;
+if the key already exist, update the value;
+if current size < cap, just add to 1st map, add key-1 to count map, and add key to 3rd map, and the used times is 1.
+	update min to 1, becasue the newly added key is used for once.
+if current size == cap, which is full, need to remove 1 before add.
+	find the key to be removed in 3rd map in the HashSet of min. Then remove from all 3 maps.
+*/
+class LFUCache {
+    Map<Integer, Integer> map;
+	Map<Integer, Integer> counts;
+	Map<Integer, LinkedHashSet<Integer>> lists;
+	int cap, min = -1;
+
+	public LFUCache(int capacity) {
+		cap = capacity;
+		map = new HashMap<>();
+		counts = new HashMap<>();
+		lists = new HashMap<>();
+		lists.put(1, new LinkedHashSet<>());
+	}
+
+	public int get(int key) {
+		if (!map.containsKey(key)) return -1;
+		int count = counts.get(key);
+		lists.get(count).remove(key);
+		if (count == min && lists.get(count).size() == 0) min++;
+		if (!lists.containsKey(++count)) lists.put(count, new LinkedHashSet<>());
+		lists.get(count).add(key);
+		counts.put(key, count);
+		return map.get(key);
+	}
+
+	public void put(int key, int value) {
+		if (cap <= 0) return;
+		if (map.containsKey(key)) {
+			map.put(key, value);
+			get(key);
+			return;
+		}
+		if (map.size() == cap) {
+			int rm = lists.get(min).iterator().next();
+			lists.get(min).remove(rm);
+			map.remove(rm);
+			counts.remove(rm);
+		}
+		map.put(key, value);
+		counts.put(key, 1);
+		min = 1;
+		lists.get(1).add(key);
+	}
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
